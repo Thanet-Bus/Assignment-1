4277203ebee5b8a4581604db63e1db55856c4838
Thanet-Bus <76712551+Thanet-Bus@users.noreply.github.com>
2021-02-26 21:12:50 +0700

    Add C file for test

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("My name is Thanet\n");
+    printf("This year, I'm 18 years old.\n");
+    printf("Next year, I am %d years old.\n",18+1);
+    printf("I'm %d years old.\n",18);
+    printf("I want %c,%d,%x,%o grade.\n",'A','A','A','A');
+    printf("I want %s grade.\n","A++");
+    printf("My friend\n");
+    printf("%-10s\t%d\n","Tewakul",20);
+    printf("%-10s\t%d\n","Kasap",18);
+    printf("%-10s\t%d\n","Panithan",18);
+    return 0;
+}
